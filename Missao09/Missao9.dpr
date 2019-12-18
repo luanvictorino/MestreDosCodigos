1a36df61f70fe533662196ed129af987d60d79d0
luanvictorino <luanhenriquevictorino2@msn.com>
2019-12-18 16:29:44 -0300

    Delphi - Escudeiro - Missão 09 - Exercício Cache de Objetos

--- Missao09/Missao9.dpr
+++ Missao09/Missao9.dpr
@@ -1,5 +1,5 @@
-//////////////////////////////////////////////////////////////
-//  Autor: Luan Victorino                                   //
+﻿//////////////////////////////////////////////////////////////
+//   Autor: Luan Victorino                                  //
 //    Date: 13/12/2019                                      //
 // Mission: Em uma aplicação console, crie uma classe       //
 //          chamada TFuncionario que armazene os dados de   //
@@ -32,6 +32,53 @@ uses
   uFuncionario in 'uFuncionario.pas',
   uCacheObjetos in 'uCacheObjetos.pas';
 
+procedure main;
+var
+  oCacheObjetos: TCacheObjetos;
+  oFuncionario1: TFuncionario;
+  oFuncionario2: TFuncionario;
 begin
-  
+  oCacheObjetos := TCacheObjetos.Create;
+  try
+    oFuncionario1 := oCacheObjetos.RetornarInstancia<TFuncionario>;
+    oFuncionario1.Nome := 'Luan';
+    oFuncionario1.Email := 'luan@db1.com.br';
+    oFuncionario1.Telefone := '(44) 3635-1914';
+    Writeln('Objeto Funcionario1 criado e valores preenchidos:');
+    Writeln('Nome: ' + oFuncionario1.Nome);
+    Writeln('Email: ' + oFuncionario1.Email);
+    Writeln('Telefone: ' + oFuncionario1.Telefone);
+
+    Writeln(EmptyStr);
+    oFuncionario2 := oCacheObjetos.RetornarInstancia<TFuncionario>;
+    Writeln('Objeto Funcionario2 criado e valores preenchidos:');
+    Writeln('Nome: ' + oFuncionario2.Nome);
+    Writeln('Email: ' + oFuncionario2.Email);
+    Writeln('Telefone: ' + oFuncionario2.Telefone);
+
+    oFuncionario2.Nome := 'Luan Victorino';
+    oFuncionario2.Email := 'luan.victorino@db1.com.br';
+    oFuncionario2.Telefone := '(44) 99736-6195';
+    Writeln(EmptyStr);
+    Writeln('Valores de Funcionario2 alterados.');
+    Writeln('Funcionário1: ');
+    Writeln('Nome: ' + oFuncionario1.Nome);
+    Writeln('Email: ' + oFuncionario1.Email);
+    Writeln('Telefone: ' + oFuncionario1.Telefone);
+    Writeln(EmptyStr);
+    Writeln('Funcionário2: ');
+    Writeln('Nome: ' + oFuncionario2.Nome);
+    Writeln('Email: ' + oFuncionario2.Email);
+    Writeln('Telefone: ' + oFuncionario2.Telefone);
+    readln;
+  finally
+    oCacheObjetos.Free;
+    FreeAndNil(oFuncionario1);
+    FreeAndNil(oFuncionario2);
+  end;
+  readln;
+end;
+
+begin
+  main;
 end.
--- Missao09/uCacheObjetos.pas
+++ Missao09/uCacheObjetos.pas
@@ -3,13 +3,57 @@ unit uCacheObjetos;
 interface
 
 uses
-  System.Generics.Collections;
+  System.SysUtils, TypInfo, System.Generics.Collections;
 
 type
   TCacheObjetos = class
+  private
     FLista: TObjectDictionary<string, TObject>;
+    function PegarNomeClasse<T: class>: String;
+    function CriarInstancia<T: class, constructor>: T;
+  public
+    constructor Create;
+    function RetornarInstancia<T: class, constructor>: T;
   end;
 
 implementation
+
+{ TClasseGenerica }
+
+constructor TCacheObjetos.Create;
+begin
+  FLista := TObjectDictionary<string, TObject>.Create;
+end;
+
+function TCacheObjetos.RetornarInstancia<T>: T;
+var
+  sNomeClasse: String;
+  bContemInstancia: Boolean;
+begin
+  sNomeClasse := PegarNomeClasse<T>;
+  bContemInstancia := FLista.ContainsKey(sNomeClasse);
+
+  if bContemInstancia then
+  begin
+    Result := FLista.Items[sNomeClasse] as T;
+    Exit;
+  end;
+
+  Result := CriarInstancia<T>;
+end;
+
+function TCacheObjetos.PegarNomeClasse<T>: String;
+begin
+  Result := GetTypeData(PTypeInfo(TypeInfo(T)))^.ClassType.QualifiedClassName;
+end;
+
+function TCacheObjetos.CriarInstancia<T>: T;
+var
+  sNomeClasse: String;
+begin
+  sNomeClasse := PegarNomeClasse<T>;
+  Result := T.Create;
+  FLista.Add(sNomeClasse, TObject(Result));
+end;
 
 end.
