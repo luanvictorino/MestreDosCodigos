6c81264a95bd61e9f9a7ce614509a0ec2475b5d5
luanvictorino <luanhenriquevictorino2@msn.com>
2019-12-19 10:05:32 -0300

    Delphi - Escudeiro - Missão 01 - Exercício Calcular fatorial

---
+++ Missao01/Missao1.dpr
@@ -0,0 +1,43 @@
+program Missao1;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+function CalcularFatorial(nValor: Int64): String;
+var
+  nContador: Int64;
+  nFatorial: Int64;
+begin
+  nFatorial := 1;
+  for nContador := nValor downto 1 do
+    nFatorial := nFatorial * nContador;
+
+  Result := IntToStr(nFatorial);
+end;
+
+procedure main;
+var
+  nValor: Int64;
+begin
+  repeat
+    Write('Informe um valor de 0 a 20 para descobrir seu fatorial: ');
+    readln(nValor);
+
+    if nValor in [0..20] then
+      Break;
+
+    Writeln('Valor inválido!');
+    Continue;
+  until False;
+
+  Writeln('O fatorial de ' + IntToStr(nValor) + ' é: ' + CalcularFatorial(nValor));
+  readln;
+end;
+
+begin
+  main;
+end.
